f6a9ab365c969ecf62adc706069319afe2b539eb
conchikeu <mozillathund@gmail.com>
2018-10-30 18:06:11 +0700

    BTVN6

---
+++ lesson 6/BTVN.js
@@ -0,0 +1,26 @@
+alert('Pop-up hear, watch me!!!!');
+// document.write('dasfk ashjkld jaklwhjkdhajsk');
+// document.getElementById('demo1').innerHTML = '<b>Changed<b>';
+// console.log('javascript');
+
+// var userName;
+// var yearsOld;
+// var bithDay;
+// var email;
+// var placeBithDay;
+
+var a = 10;
+var b = 20;
+var c = 30;
+var d = 12;
+
+document.write('a + b = ');
+document.write(a + b);
+document.write('c - d = ');
+document.write(c + d);
+
+function functionName(){
+	document.write('Mark');
+}
+//goi ham;
+// function();
